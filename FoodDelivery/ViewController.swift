4a39cddd6d8924c623791562733628be64793a30
Ali Jawad <jawad.jd@hotmail.com>
2021-09-07 15:09:23 +0500

    First commit

---
+++ FoodDelivery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Ali Jawad on 07/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+}
+
