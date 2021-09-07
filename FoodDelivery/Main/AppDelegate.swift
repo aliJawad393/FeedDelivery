45acc30719cf76457776f71f07609c52da09f39f
Ali Jawad <jawad.jd@hotmail.com>
2021-09-07 16:05:40 +0500

    clean up appDelegate and sceneDelegate

---
+++ FoodDelivery/Main/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  FoodDelivery
+//
+//  Created by Ali Jawad on 07/09/2021.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {}
+
---
+++ FoodDelivery/Main/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  FoodDelivery
+//
+//  Created by Ali Jawad on 07/09/2021.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        window?.windowScene = windowScene
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
+
